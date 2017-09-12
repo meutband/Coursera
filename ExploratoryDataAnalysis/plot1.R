f57041849d5b5d19b758369a8ac4cf8be28f7f99
Mark Evans <meutband@gmail.com>
2017-09-12 09:51:17 -0500

    Add files via upload

---
+++ ExploratoryDataAnalysis/plot1.R
@@ -0,0 +1,15 @@
+## setwd('Desktop/Coursera/DataScienceSpecialization/ExploratoryDataAnalysis/Week4/')
+
+## import the data (changed the folder name to make it more readable)
+NEI <- readRDS("FNEI_data/summarySCC_PM25.rds")
+
+## create array of the total emisions per year
+TotalByYear <- tapply(NEI$Emissions, NEI$year, sum)
+
+## plot the total emisions against the year, save the plot as 'plot1.png'
+png("plot1.png")
+plot(names(TotalByYear), TotalByYear, type='l', xlab='Year', ylab='Total Emissions (tons)', 
+     main='The Total Emissions of PM2.5 per Year (in tons)')
+dev.off()
+
+## The total amount of pm2.5 emissions is decreasing between the years of 1999 and 2008.
